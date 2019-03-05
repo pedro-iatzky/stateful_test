43da1cf3f664ec2c4960eca8e64b77a9e2f88787
piatzky <iatzkypedro@gmail.com>
2019-03-05 14:04:18 -0300

    Add setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(
+    name="stateful_test",
+    version="0.1",
+    author="Pedro Iatzky",
+    author_email="iatzkypedro@gmail.com",
+    description="A simpleframework for performing integration tests",
+    packages=["stateful_test"],
+    python_requires=">=3.5",
+    extras_require={
+        'dotenv': ['python-dotenv'],
+        'dev': [
+            'pytest>=3'
+        ]
+    }
+)
